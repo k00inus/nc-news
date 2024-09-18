95d5592ccb022f3707197587e58e02d203a6b9d2
kinus00 <usuncodes@gmail.com>
2024-09-18 10:33:44 +0100

    add fixes 7-core-vote-on-an-article

--- src/components/UpdateLikes.jsx
+++ src/components/UpdateLikes.jsx
@@ -1,11 +1,17 @@
 import { AiFillLike, AiFillDislike } from "react-icons/ai";
 import { patchArticleById } from "../utils/utils";
+import { useState } from "react";
 const UpdateLikes = ({ values: [article, setArticle] }) => {
+  const [canVote, setCanVote] = useState(undefined);
+
   function handleLikes() {
+    setCanVote(true);
+
     setArticle((prevData) => ({
       ...prevData,
       votes: article.votes + 1,
     }));
+
     patchArticleById(article, {
       inc_votes: 1,
     }).catch((err) => {
@@ -17,6 +23,7 @@ const UpdateLikes = ({ values: [article, setArticle] }) => {
   }
 
   function handleDislikes() {
+    setCanVote(false);
     setArticle((prevData) => ({
       ...prevData,
       votes: article.votes - 1,
@@ -34,15 +41,16 @@ const UpdateLikes = ({ values: [article, setArticle] }) => {
   return (
     <>
       <p className="flex text-gray-500">
-        <AiFillLike
-          className="mr-1 mt-[2px] text-blue-500"
-          onClick={handleLikes}
-        />
+        <button onClick={handleLikes} disabled={canVote}>
+          <AiFillLike className="mr-1 text-blue-500" />
+        </button>
         {article.votes}
-        <AiFillDislike
-          className="ml-1 mt-[2px] text-blue-500"
+        <button
           onClick={handleDislikes}
-        />
+          disabled={canVote === false && canVote !== undefined}
+        >
+          <AiFillDislike className="ml-1 text-blue-500" />
+        </button>
       </p>
     </>
   );
